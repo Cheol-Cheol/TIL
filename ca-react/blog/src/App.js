e1f70e4f56da07195949d7441a66ddf1d118c8cc
Cheol-Cheol <dream174822@gmail.com>
2022-06-03 23:42:12 +0900

    ✏️ State

--- ca-react/blog/src/App.js
+++ ca-react/blog/src/App.js
@@ -1,15 +1,62 @@
+/*eslint-disable*/
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  // 서버에서 가져온 데이터로 생각하자
-  let post = "강남 우동 맛집";
+  // state 왜 써야함?
+  // state가 변경되면 관련 HTML은 자동 재렌더링된다.
+  let [글제목, 글제목변경] = useState([
+    "가자 코트 추천",
+    "다자 코트 추천",
+    "나동 코트 추천",
+  ]);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{ color: "red", fontSize: "16px" }}>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{post}</h4>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy.sort();
+          글제목변경(copy);
+        }}
+      >
+        가나다순
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy[0] = "여여 코트 변경";
+          글제목변경(copy);
+        }}
+      >
+        버튼
+      </button>
+      <div className="list">
+        <h4>
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {따봉}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
