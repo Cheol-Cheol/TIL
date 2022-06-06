e3ae34f6c5b26bca4bc12854b6deeb0b5099d385
Cheol-Cheol <dream174822@gmail.com>
2022-06-06 10:16:35 +0900

    ✏️ N개의 상세페이지 - useParams()

--- ca-react/shop/src/App.js
+++ ca-react/shop/src/App.js
@@ -3,9 +3,12 @@ import { useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import data from "./data";
 import bg from "./img/bg.png";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/Detail";
 
 function App() {
   let [shoes, setShoes] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -13,21 +16,65 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#cart">Cart</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Detail
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg" style={{ backgroundImage: `url(${bg})` }}></div>
 
-      {/* grid는 오리지널 부트스트랩 코드가 더 간단명료해서 사용했음 */}
-      <div className="container">
-        <div className="row">
-          {shoes.map((shoe, i) => {
-            return <Card shoe={shoe} i={i} key={i} />;
-          })}
-        </div>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div
+                className="main-bg"
+                style={{ backgroundImage: `url(${bg})` }}
+              ></div>
+
+              {/* grid는 오리지널 부트스트랩 코드가 더 간단명료해서 사용했음 */}
+              <div className="container">
+                <div className="row">
+                  {shoes.map((shoe, i) => {
+                    return <Card shoe={shoe} i={i} key={i} />;
+                  })}
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+        {/* Nested Routes */}
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버 페이지</div>} />
+          <Route path="location" element={<div>위치 페이지</div>} />
+        </Route>
+        <Route
+          path="/event"
+          element={
+            <div>
+              <h4>오늘의 이벤트</h4>
+              <Outlet></Outlet>
+            </div>
+          }
+        >
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
+        <Route path="*" element={<div>없는 페이지에요</div>} />
+      </Routes>
     </div>
   );
 }
@@ -40,6 +87,15 @@ function Card(props) {
       />
       <h4>{props.shoe.title}</h4>
       <p>{props.shoe.price}</p>
+    </div>
+  );
+}
+function About() {
+  return (
+    <div>
+      <h4>회사정보페이지</h4>
+      {/* Outlet : Nested Routes 들을 보여줄 구멍역할 */}
+      <Outlet></Outlet>
     </div>
   );
 }
