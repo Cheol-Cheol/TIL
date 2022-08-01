e03e5a828d11456e592186d6538545b3f965c481
Cheol-Cheol <dream174822@gmail.com>
2022-08-01 22:49:41 +0900

    ✏️ class 기반 컴포넌트 - State 업데이트

--- dc-react/habit-tracker/src/components/Habits.jsx
+++ dc-react/habit-tracker/src/components/Habits.jsx
@@ -12,14 +12,31 @@ export default class Habits extends Component {
 
   handleIncrement = (habit) => {
     console.log(`handleIncrement ${habit.name}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
   };
 
   handleDecrement = (habit) => {
     console.log(`handleDecrement ${habit.name}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
   };
 
   handleDelete = (habit) => {
     console.log(`handleDelete ${habit.name}`);
+    // const habits = [...this.state.habits];
+    // const index = habits.indexOf(habit);
+    // habits.splice(index);
+    // this.setState({ habits });
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    // setState({habits: habits})
+    // 상단의 state의 데이터 형식에 따라 만들어준다.(객체)
+    this.setState({ habits });
   };
 
   render() {
