007f641c11912b5ac810f2fe5285c263cf632190
Cheol-Cheol <dream174822@gmail.com>
2022-08-01 23:14:26 +0900

    ✏️ App component 만들기

--- dc-react/habit-tracker/src/app.jsx
+++ dc-react/habit-tracker/src/app.jsx
@@ -1,8 +1,49 @@
+import { Component } from "react";
 import "./App.css";
 import Habits from "./components/Habits";
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+
+  onIncrement = (habit) => {
+    console.log(`handleIncrement ${habit.name}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  onDecrement = (habit) => {
+    console.log(`handleDecrement ${habit.name}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  onDelete = (habit) => {
+    console.log(`handleDelete ${habit.name}`);
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <Habits
+        habits={this.state.habits}
+        onIncrement={this.onIncrement}
+        onDecrement={this.onDecrement}
+        onDelete={this.onDelete}
+      />
+    );
+  }
 }
 
 export default App;
--- dc-react/habit-tracker/src/components/Habits.jsx
+++ dc-react/habit-tracker/src/components/Habits.jsx
@@ -2,58 +2,24 @@ import React, { Component } from "react";
 import Habit from "./Habit";
 
 export default class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: "Reading", count: 0 },
-      { id: 2, name: "Running", count: 0 },
-      { id: 3, name: "Coding", count: 0 },
-    ],
-  };
-
-  handleIncrement = (habit) => {
-    console.log(`handleIncrement ${habit.name}`);
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({ habits });
-  };
-
-  handleDecrement = (habit) => {
-    console.log(`handleDecrement ${habit.name}`);
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
-    this.setState({ habits });
-  };
-
-  handleDelete = (habit) => {
-    console.log(`handleDelete ${habit.name}`);
-    // const habits = [...this.state.habits];
-    // const index = habits.indexOf(habit);
-    // habits.splice(index);
-    // this.setState({ habits });
-    const habits = this.state.habits.filter((item) => item.id !== habit.id);
-    // setState({habits: habits})
-    // 상단의 state의 데이터 형식에 따라 만들어준다.(객체)
-    this.setState({ habits });
-  };
-
   render() {
     return (
-      <ul>
-        {this.state.habits.map((habit) => {
-          return (
-            <Habit
-              habit={habit}
-              key={habit.id}
-              onIncrement={this.handleIncrement}
-              onDecrement={this.handleDecrement}
-              onDelete={this.handleDelete}
-            />
-          );
-        })}
-      </ul>
+      <div className="habits">
+        <ul>
+          {this.props.habits.map((habit) => {
+            return (
+              <Habit
+                key={habit.id}
+                habit={habit}
+                onIncrement={this.props.onIncrement}
+                onDecrement={this.props.onDecrement}
+                onDelete={this.props.onDelete}
+              />
+            );
+          })}
+          <button className="habits-reset">Reset All</button>
+        </ul>
+      </div>
     );
   }
 }
